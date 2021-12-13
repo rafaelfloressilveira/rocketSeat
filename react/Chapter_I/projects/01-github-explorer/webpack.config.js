4b0d753c7cdc4c34435394f0f572df9b48ff8811
rafaelfloressilveira <rafaelfloressilveira2@gmail.com>
2021-12-13 17:38:51 -0300

    configuration react

--- react/Chapter_I/projects/01-github-explorer/webpack.config.js
+++ react/Chapter_I/projects/01-github-explorer/webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path') // colocamos para que seja alterado conforme o sistema operacional
+const HtmlWebpackPlugin = require ('html-webpack-plugin') // importar plugin webpack
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: 'development',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'), // arquivo de entrada
     output: {
         path: path.resolve(__dirname, 'dist'), // config saida
@@ -10,14 +14,30 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.jsx'],
     },
+
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
+
     module: {
         rules: [
             {
                 test: /\.jsx$/, // importo o arquivo jsx
                 exclude: /node_modules/,
                 use: 'babel-loader' // converto o arquivo com babel
+            },
+            {
+                test: /\.scss$/, // importo o arquivo css
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader'], // converto o arquivo com style e css loader
             }
         ],
     }
 
-};
+}
