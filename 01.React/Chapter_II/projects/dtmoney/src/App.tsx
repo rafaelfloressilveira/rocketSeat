dc14a4b87131d4331c26a8a1a5007cc45be28f85
rafaelfloressilveira <rafaelfloressilveira2@gmail.com>
2021-12-16 17:27:49 -0300

    Input styles dtmoney

--- 01.React/Chapter_II/projects/dtmoney/src/App.tsx
+++ 01.React/Chapter_II/projects/dtmoney/src/App.tsx
@@ -1,7 +1,11 @@
+import { GlobalStyle } from "./style/global";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1> Hello World </h1>
+
+    <GlobalStyle />
 
     </div>
   );
