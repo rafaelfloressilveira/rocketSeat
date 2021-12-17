fef289002599b08eccc601d2135af771829f4530
rafaelfloressilveira <rafaelfloressilveira2@gmail.com>
2021-12-16 23:10:28 -0300

    Creat components

--- 01.React/Chapter_II/projects/dtmoney/src/App.tsx
+++ 01.React/Chapter_II/projects/dtmoney/src/App.tsx
@@ -1,12 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./style/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1> Hello World </h1>
-
-    <GlobalStyle />
-
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
